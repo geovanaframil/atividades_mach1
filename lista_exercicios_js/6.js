3d411f6003241534d5b05d8ec59df310fb051023
Geovana Framil <geovanaframil@hotmail.com>
2022-09-04 07:20:30 -0300

    Solução do exercício 6 conforme solicitado em atv

--- lista_exercicios_js/6.js
+++ lista_exercicios_js/6.js
@@ -4,3 +4,15 @@ Exemplo:
 somarNumeros([10, 10, 10]) // retornará 30
 somarNumeros([15, 15, 15, 15]) // retornará 60
 */
+
+function somarNumeros(arrayNumeros) {
+    arrayNumeros;
+    let total = 0;
+
+    for(let i = 0; i < arrayNumeros.length; i++) {
+        total += arrayNumeros[i]
+    }
+    return total;
+}
+console.log(somarNumeros([10, 10, 10]))
+console.log(somarNumeros([15, 15, 15, 15]))
