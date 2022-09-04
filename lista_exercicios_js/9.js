314b76a5f211545643a9c54fd3889f42dfbc6cb5
Geovana Framil <geovanaframil@hotmail.com>
2022-09-04 08:06:39 -0300

    Resolução da atv 9 conforme solicitado

--- lista_exercicios_js/9.js
+++ lista_exercicios_js/9.js
@@ -5,3 +5,17 @@ maiorOuIgual(0, 0) // retornará true
 maiorOuIgual(0, "0") // retornará false
 maiorOuIgual(5, 1) // retornará true
 */
+
+function maiorOuIgual(num1, num2) {
+    if(num1 >= num2 && typeof(num1) === typeof(num2)) {
+        console.log(true)
+    } else {
+        console.log(false)
+    }
+    return
+}
+
+maiorOuIgual(0, 0)
+maiorOuIgual(0, '0')
+maiorOuIgual(5, 1)
+maiorOuIgual(5, '5')
