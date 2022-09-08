891aafeff300561a084ba05730d049163e95f428
Geovana Framil <geovanaframil@hotmail.com>
2022-09-08 19:44:35 -0300

    Cria matriz da tabuada do 2 e dois alerts

---
+++ ja_aula_02_matrizes/aula02.js
@@ -0,0 +1,18 @@
+let tabuada = [
+  [2, 0, 0],
+  [2, 1, 2],
+  [2, 2, 4],
+  [2, 3, 6],
+  [2, 4, 8],
+  [2, 5, 10],
+  [2, 6, 12],
+  [2, 7, 14],
+  [2, 8, 16],
+  [2, 9, 18],
+  [2, 10, 20],
+];
+
+alert("Representação da tabuada do número 2");
+alert(
+  "Analisando os números e a sequencia destes, pude notar que era a tabuada do número dois, porém sem os sinais de multiplicação e igualdade!"
+);
