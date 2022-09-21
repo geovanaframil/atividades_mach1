d96ce83cc6023cb0286bfc790072345e12f6282d
Geovana Framil <geovanaframil@hotmail.com>
2022-09-21 09:13:08 -0300

    Cria variável para exibir no conceito NodeList tudo que há no documento html com a tag p

--- js_aula_08_DOM/assets/js/dom.js
+++ js_aula_08_DOM/assets/js/dom.js
@@ -1,12 +1,15 @@
 let qtdFilhosBody = document.body.childElementCount;
-console.log(qtdFilhosBody)
+console.log(qtdFilhosBody);
 
 let verificaQtdFilhos = (qtdFilhos) => {
-    let qtdFilhosMenorQueCinco = false;
-    if(qtdFilhos > 5) {
-        qtdFilhosMenorQueCinco = true;
-    }
-    return qtdFilhosMenorQueCinco;
-}
+  let qtdFilhosMenorQueCinco = false;
+  if (qtdFilhos > 5) {
+    qtdFilhosMenorQueCinco = true;
+  }
+  return qtdFilhosMenorQueCinco;
+};
 
-console.log(verificaQtdFilhos(qtdFilhosBody))
+console.log(verificaQtdFilhos(qtdFilhosBody));
+
+let filhosP = document.querySelectorAll("p");
+console.log(filhosP)
