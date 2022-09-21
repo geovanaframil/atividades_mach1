81ed9a85fa0cc220710424469e4456b838d60cf1
Geovana Framil <geovanaframil@hotmail.com>
2022-09-21 09:04:00 -0300

    Cria função que verifica quantidade de filhos no body do html e retorna um boleano

--- js_aula_08_DOM/assets/js/dom.js
+++ js_aula_08_DOM/assets/js/dom.js
@@ -1,2 +1,12 @@
 let qtdFilhosBody = document.body.childElementCount;
 console.log(qtdFilhosBody)
+
+let verificaQtdFilhos = (qtdFilhos) => {
+    let qtdFilhosMenorQueCinco = false;
+    if(qtdFilhos > 5) {
+        qtdFilhosMenorQueCinco = true;
+    }
+    return qtdFilhosMenorQueCinco;
+}
+
+console.log(verificaQtdFilhos(qtdFilhosBody))
