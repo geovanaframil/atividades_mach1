08c1c35d821df1392c33650051302b312f38cdb7
Geovana Framil <geovanaframil@hotmail.com>
2022-09-27 21:37:31 -0300

    Linka js no html e chama função para limpar formulário após clique no botão

--- js_aula_11_DOM/assets/js/dom.js
+++ js_aula_11_DOM/assets/js/dom.js
@@ -5,9 +5,47 @@ let phoneElement = document.querySelector("#phone");
 let saveButtonElement = document.querySelector("#btn-save");
 let concludeButtonElement = document.querySelector("#btn-conclude");
 
+let completedForm = [];
+
+let cleanForm = () => {
+    nameElement.value = '';
+    emailElement.value = '';
+    phoneElement.value = '';
+}
+
 let validField = (nameField, emailField, phoneField) => {
-  if (nameField.length > 2 && emailField.length > 2 && phoneField.length == 11) {
+  if (
+    nameField.length > 2 &&
+    emailField.length > 2 &&
+    phoneField.length == 11
+  ) {
     return true;
   }
-  return false
+  return false;
 };
+
+let saveForm = () => {
+  let nameValue = nameElement.value;
+  let emailValue = emailElement.value;
+  let phoneValue = phoneElement.value;
+
+  let validForm = validField(nameValue, emailValue, phoneValue);
+
+  let form = {
+    name: nameValue,
+    email: emailValue,
+    phone: phoneValue,
+  };
+
+  if (validForm) {
+    completedForm.push(form);
+    console.log(completedForm);
+  } else {
+    console.log("ERRO!");
+  }
+
+  cleanForm();
+};
+
+
+saveButtonElement.addEventListener('click', saveForm)
