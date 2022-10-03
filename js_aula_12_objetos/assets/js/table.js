e08f3cdaaa6a922b8fa06e62156b666c68feb01d
Geovana Framil <geovanaframil@hotmail.com>
2022-10-03 06:24:00 -0300

    Cria página html e lista de objetos js

---
+++ js_aula_12_objetos/assets/js/table.js
@@ -0,0 +1,32 @@
+let tBodyElement = document.querySelector('#result');
+
+const listaUsuarios = [
+  {
+    id: 1,
+    nome: "Thiago Pereira",
+    sexo: "Masculino",
+    idade: 20,
+    email: "fulanodetal@termail.com",
+  },
+  {
+    id: 2,
+    nome: "Thaías Lins",
+    sexo: "Feminino",
+    idade: 19,
+    email: "thaisdetal@gtailme.com",
+  },
+  {
+    id: 3,
+    nome: "Mariana Ferreira",
+    sexo: "Feminino",
+    idade: 25,
+    email: "marianaferreira@tailme.com",
+  },
+  {
+    id: 4,
+    nome: "Pedro Ivo Silva",
+    sexo: "Feminino",
+    idade: 28,
+    email: "manuelatrait@gtepmail.com",
+  },
+];
