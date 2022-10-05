b535aa91c0c440c7eb1f30cde096b3cc58c34134
Geovana Framil <geovanaframil@hotmail.com>
2022-10-05 13:26:11 -0300

    Adiciona função para pesquisar por propriedade do objeto e exibir o resultado no DOM

--- js_aula_14_objetos/assets/js/script.js
+++ js_aula_14_objetos/assets/js/script.js
@@ -34,25 +34,50 @@ const pesquisa = document.getElementById("pesquisa"); //Captura elemento input a
 const btnPesquisar = document.getElementById("btnPesquisar"); //Captura elemento botão pesquisar através do ID btnPesquisar
 const btnLimpar = document.getElementById("btnLimpar"); //Captura elemento botão limpar através do ID btnLimpar
 
-let exibirTabelaUsuarios = (pessoa) => {
-  //Função para exibir na tela linha da tabela com os dados do objeto listaUsuarios
-  let elementoTBody = document.getElementById("resultado"); //Captura elemento tbody através do ID resultado
-  let linhaTabela = `<tr>
-                        <td>${pessoa.id}</td>
-                        <td>${pessoa.nome}</td>
-                        <td>${pessoa.sexo}</td>
-                        <td>${pessoa.idade}</td>
-                        <td>${pessoa.email}</td>
-                        </tr>`; //Variável que recebe uma template string com tags html onde cada td recebe o objeto pessoa com a propriedade especificado após o ponto;
-  elementoTBody.innerHTML += linhaTabela; //Exibe no DOM a variável linha tabela dentro do elementoTBody
+let listarUsuariosNaTabela = (filtrados = undefined) => {
+  let trTds = "";
+  let lista = [];
+  let elementoTBody = document.getElementById("resultado");
+
+  lista = filtrados === undefined ? listaUsuarios : filtrados;
+
+  lista.forEach((usuario) => {
+    trTds += `<tr><td>${usuario.id}</td>`;
+    trTds += `<td>${usuario.nome}</td>`;
+    trTds += `<td>${usuario.sexo}</td>`;
+    trTds += `<td>${usuario.idade}</td>`;
+    trTds += `<td>${usuario.email}</td></tr>`;
+  });
+  elementoTBody.innerHTML = trTds;
+
+  if (lista.length === 0) {
+    elementoTBody.innerHTML = `<tr><td colspan="5">Nenhum dado encontrado</td></tr>`;
+  }
+};
+
+let filtraDados = () => {
+  let filtrados = [];
+
+  if (pesquisa.value.toLowerCase() !== "") {
+    filtrados = listaUsuarios.filter(
+      (usuario) =>
+        usuario.id.toString().indexOf(pesquisa.value) > -1 ||
+        usuario.nome.toLowerCase().indexOf(pesquisa.value.toLowerCase()) > -1 ||
+        usuario.sexo.toLowerCase().indexOf(pesquisa.value.toLowerCase()) > -1 ||
+        usuario.idade.toString().indexOf(pesquisa.value) > -1 ||
+        usuario.email.toLowerCase().indexOf(pesquisa.value.toLowerCase()) > -1
+    );
+  } else {
+    filtrados = listaUsuarios;
+  }
+  listarUsuariosNaTabela(filtrados);
 };
 
 let limpaFormulario = () => {
   pesquisa.value = "";
+  listarUsuariosNaTabela(listaUsuarios);
 };
 
-listaUsuarios.forEach(exibirTabelaUsuarios); //Percorre cada objeto do array de objetos listaUsuario e executa a função callback exibirTabelaUsuarios
-
-window.addEventListener("onload", exibirTabelaUsuarios); //Executa a função exibirTabelaUsuarios no momento em que a página é carregada
-
+window.addEventListener("onload", listarUsuariosNaTabela()); //Executa a função exibirTabelaUsuarios no momento em que a página é carregada
+btnPesquisar.addEventListener("click", filtraDados);
 btnLimpar.addEventListener("click", limpaFormulario); //Executa a função limpaFormulário no momento em que o botão Limpar recebe o evento de click
