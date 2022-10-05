f8925f122dd6ffceade7ad853729652feca1d99d
Geovana Framil <geovanaframil@hotmail.com>
2022-10-05 10:22:49 -0300

    Cria função para limpar input quando o usuário clicar no botão Limpar

--- js_aula_14_objetos/assets/js/script.js
+++ js_aula_14_objetos/assets/js/script.js
@@ -1,4 +1,5 @@
-let listaUsuarios = [ //Lista de objetos usuarios
+let listaUsuarios = [
+  //Lista de objetos usuarios
   {
     id: 1,
     nome: "Thiago Pereira",
@@ -29,6 +30,29 @@ let listaUsuarios = [ //Lista de objetos usuarios
   },
 ];
 
-const pesquisa = document.getElementById("pesquisa");//Captura elemento input através do ID pesquisa
-const btnPesquisar = document.getElementById("btnPesquisar");//Captura elemento botão pesquisar através do ID btnPesquisar
-const btnLimpar = document.getElementById("btnLimpar");//Captura elemento botão limpar através do ID btnLimpar
+const pesquisa = document.getElementById("pesquisa"); //Captura elemento input através do ID pesquisa
+const btnPesquisar = document.getElementById("btnPesquisar"); //Captura elemento botão pesquisar através do ID btnPesquisar
+const btnLimpar = document.getElementById("btnLimpar"); //Captura elemento botão limpar através do ID btnLimpar
+
+let exibirTabelaUsuarios = (pessoa) => {
+  //Função para exibir na tela linha da tabela com os dados do objeto listaUsuarios
+  let elementoTBody = document.getElementById("resultado"); //Captura elemento tbody através do ID resultado
+  let linhaTabela = `<tr>
+                        <td>${pessoa.id}</td>
+                        <td>${pessoa.nome}</td>
+                        <td>${pessoa.sexo}</td>
+                        <td>${pessoa.idade}</td>
+                        <td>${pessoa.email}</td>
+                        </tr>`; //Variável que recebe uma template string com tags html onde cada td recebe o objeto pessoa com a propriedade especificado após o ponto;
+  elementoTBody.innerHTML += linhaTabela; //Exibe no DOM a variável linha tabela dentro do elementoTBody
+};
+
+let limpaFormulario = () => {
+  pesquisa.value = "";
+};
+
+listaUsuarios.forEach(exibirTabelaUsuarios); //Percorre cada objeto do array de objetos listaUsuario e executa a função callback exibirTabelaUsuarios
+
+window.addEventListener("onload", exibirTabelaUsuarios); //Executa a função exibirTabelaUsuarios no momento em que a página é carregada
+
+btnLimpar.addEventListener("click", limpaFormulario); //Executa a função limpaFormulário no momento em que o botão Limpar recebe o evento de click
