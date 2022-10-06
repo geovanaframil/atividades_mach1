11b00e1dcf004b02a508578c61f3edf6b149d0f0
Geovana Framil <geovanaframil@hotmail.com>
2022-10-05 21:07:21 -0300

    Adiciona comentários

--- js_aula_14_objetos/assets/js/script.js
+++ js_aula_14_objetos/assets/js/script.js
@@ -34,31 +34,31 @@ const pesquisa = document.getElementById("pesquisa"); //Captura elemento input a
 const btnPesquisar = document.getElementById("btnPesquisar"); //Captura elemento botão pesquisar através do ID btnPesquisar
 const btnLimpar = document.getElementById("btnLimpar"); //Captura elemento botão limpar através do ID btnLimpar
 
-let listarUsuariosNaTabela = (filtrados = undefined) => {
-  let trTds = "";
-  let lista = [];
-  let elementoTBody = document.getElementById("resultado");
+let listarUsuariosNaTabela = (filtrados = undefined) => {//Função que exibe tabela no DOM
+  let trTds = "";//Variável recebe string vazia para que possa ser alterada posteriormente
+  let lista = [];//Cria variável que inicializa com array vazio;
+  let elementoTBody = document.getElementById("resultado");//Captura elemento tbody através do ID resultado
 
-  lista = filtrados === undefined ? listaUsuarios : filtrados;
+  lista = filtrados === undefined ? listaUsuarios : filtrados;//Lista recebe condição que testa se o parâmetro filtrados é undefined e retorna listaUsuarios caso sim, caso não, retorna o parâmetro filtrados;
 
-  lista.forEach((usuario) => {
+  lista.forEach((usuario) => {//Percorre array lista e devolve template string com tags html com cada propriedade do objeto que foi passada
     trTds += `<tr><td>${usuario.id}</td>`;
     trTds += `<td>${usuario.nome}</td>`;
     trTds += `<td>${usuario.sexo}</td>`;
     trTds += `<td>${usuario.idade}</td>`;
     trTds += `<td>${usuario.email}</td></tr>`;
   });
-  elementoTBody.innerHTML = trTds;
+  elementoTBody.innerHTML = trTds;//Insere no html na tag que foi passada o elementoTBody a variável trTds;
 
-  if (lista.length === 0) {
+  if (lista.length === 0) {//Testa se o cumprimento do array lista é vazio, e caso seja, insere no html a template string abaixo;
     elementoTBody.innerHTML = `<tr><td colspan="5">Nenhum dado encontrado</td></tr>`;
   }
 };
 
-let filtraDados = () => {
-  let filtrados = [];
+let filtraDados = () => {//Função que filtra os dados solicitados;
+  let filtrados = [];//Variável que recebe um array vazio;
 
-  if (pesquisa.value.toLowerCase() !== "") {
+  if (pesquisa.value.toLowerCase() !== "") {//Verifica se o valor do input (convertido já em minúsculo) é diferente de vazio;
     filtrados = listaUsuarios.filter(
       (usuario) =>
         usuario.id.toString().indexOf(pesquisa.value) > -1 ||
@@ -78,6 +78,6 @@ let limpaFormulario = () => {
   listarUsuariosNaTabela(listaUsuarios);
 };
 
-window.addEventListener("onload", listarUsuariosNaTabela()); //Executa a função exibirTabelaUsuarios no momento em que a página é carregada
-btnPesquisar.addEventListener("click", filtraDados);
+window.addEventListener("onload", listarUsuariosNaTabela()); //Executa a função listarUsuariosNaTabela no momento em que a página é carregada
+btnPesquisar.addEventListener("click", filtraDados);//Executa a função filtraDados no momento em que o botão Pesquisar recebe o evento de click
 btnLimpar.addEventListener("click", limpaFormulario); //Executa a função limpaFormulário no momento em que o botão Limpar recebe o evento de click
