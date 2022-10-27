b969f0c82478325ad4575a80763931f839cf48ad
Geovana Framil <geovanaframil@hotmail.com>
2022-10-27 19:18:23 -0300

    Atividade dois de recursividade

---
+++ js_avancado_aula_01_pt2/index.js
@@ -0,0 +1,81 @@
+const itens = [
+  {
+    nome: "Menu 1",
+    itens: [
+      {
+        nome: "Menu 1.1",
+        itens: [],
+      },
+      {
+        nome: "Menu 1.2",
+        itens: [],
+      },
+      {
+        nome: "Menu 1.3",
+        itens: [
+          {
+            nome: "Menu 1.3.1",
+            itens: [],
+          },
+        ],
+      },
+    ],
+  },
+  {
+    nome: "Menu 2",
+    itens: [
+      {
+        nome: "Menu 2.1",
+        itens: [
+          {
+            nome: "Menu 2.1.1",
+            itens: [
+              {
+                nome: "Menu 2.1.1.1",
+                itens: [
+                  {
+                    nome: "Menu 2.1.1.1.1",
+                    itens: [],
+                  },
+                  {
+                    nome: "Menu 2.1.1.1.2",
+                    itens: [],
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            nome: "Menu 2.1.2",
+            itens: [],
+          },
+        ],
+      },
+      {
+        nome: "Menu 2.2",
+        itens: [
+          {
+            nome: "Menu 2.2.1",
+            itens: [],
+          },
+        ],
+      },
+    ],
+  },
+];
+
+let funcaoRecursiva = (arrItens, nomeItem) => {
+  for (let i = 0; i < arrItens.length; i++) {
+    if (arrItens[i].nome === nomeItem) {
+      return nomeItem;
+    } else if (arrItens[i].itens.length > 0) {
+      let saida = funcaoRecursiva(arrItens[i].itens, nomeItem);
+      if (saida !== undefined) {
+        return `${arrItens[i].nome} > ${saida}`;
+      }
+    }
+  }
+};
+
+console.log(funcaoRecursiva(itens, "Menu 2.1.1.1.2"));
+console.log(funcaoRecursiva(itens, "Menu 3.1"));
