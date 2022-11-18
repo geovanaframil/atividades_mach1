ba1b25a85561fcdf890161931fe96e1417c843a8
Geovana Framil <geovanaframil@hotmail.com>
2022-11-18 19:49:11 -0300

    Edita função buscarPorNomeOuTodos

--- js_avancado_aula_10/busca.js
+++ js_avancado_aula_10/busca.js
@@ -1,9 +1,22 @@
 let inputMatricula = document.querySelector("#matricula");
 let btnBuscarMatricula = document.querySelector("#btnBuscarMatricula");
+
 let labelMatricula = document.querySelector("#matricula-aluno");
 let labelNome = document.querySelector("#nome-aluno");
+
+let radioBuscaNome = document.querySelector("#opt-busca-nome");
+let radioBuscaTodos = document.querySelector("#opt-busca-todos");
+let campoInputNome = document.querySelector("#pn-filter-nome");
+
 let btnBuscar = document.querySelector("#btnBuscar");
+
 let divDados = document.querySelector("#dados");
+
+radioBuscaNome.addEventListener("change", () => {
+  if (radioBuscaNome.checked) {
+    campoInputNome.removeAttribute("style");
+  }
+});
 
 const buscarPorMatricula = () => {
   let valorInputMatricula = inputMatricula.value;
@@ -22,25 +35,32 @@ const buscarPorMatricula = () => {
   );
 };
 
-const buscar = () => {
-  let inputChecked = document.querySelector(
-    'input[name="tipo-busca"]:checked'
-  ).value;
+const buscarPorNomeOuTodos = () => {
+  divDados.innerHTML = "Carregando";
+  let url = `http://localhost:3000/aluno/todos`;
 
-  fetch(`http://localhost:3000/aluno/todos`).then((response) => {
+  if (radioBuscaNome.checked) {
+    let inputNome = document.querySelector("#nome").value;
+
+    url = `http://localhost:3000/aluno/search?nome=${inputNome}`;
+  }
+
+  fetch(url).then((response) => {
     if (response.ok) {
-      response.json().then((jsonData) => {
-        if (jsonData.length >= 1) {
-          jsonData.forEach((element) => {
-            divDados.innerHTML += `${element.matricula} - ${element.nome} `;
-          });
-        } else {
-            divDados.innerHTML = `Nenhuma informação encontrada`
-        }
-      });
+      divDados.innerHTML = "";
+      return response.json();
     }
+  })
+  .then((jsonData) => {
+    if (jsonData.length == 0) {
+      divDados.innerHTML = "Nenhuma informação encontrada";
+      return;
+    }
+    jsonData.forEach((element) => {
+      divDados.innerHTML += `<p>${element.matricula} - ${element.nome}`;
+    });
   });
 };
 
 btnBuscarMatricula.addEventListener("click", buscarPorMatricula);
-btnBuscar.addEventListener("click", buscar);
+btnBuscar.addEventListener("click", buscarPorNomeOuTodos);
