3f045fc767f8c514aa9c64204cd46a3b6a20c05b
Geovana Framil <geovanaframil@hotmail.com>
2023-04-05 21:01:45 -0300

    Inicia atividade da aula 5 com método put

---
+++ node/aula5_atv_put/src/interfaces/product.ts
@@ -0,0 +1,8 @@
+export interface IProduct {
+  productName: string;
+  productDescription: string;
+  productCategory: string;
+  productCost: number;
+  productTags: Array<string>;
+  productRelated: Array<number>;
+}
---
+++ node/aula5_atv_put/src/server.ts
@@ -0,0 +1,3 @@
+import express from "express";
+import { Router, Request, Response } from "express";
+import { IProduct } from "./interfaces/product";
