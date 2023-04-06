a648fe239c9fe01f8f24573b905a57974fe4227b
Geovana Framil <geovanaframil@hotmail.com>
2023-04-06 20:09:49 -0300

    Inicia rota get

---
+++ node/aula5_atv_put/src/enums/messages.ts
@@ -0,0 +1,3 @@
+export enum messages {
+  unregisteredClient = "Não há clientes cadastrados",
+}
--- node/aula5_atv_put/src/server.ts
+++ node/aula5_atv_put/src/server.ts
@@ -1,11 +1,22 @@
 import express from "express";
 import { Router, Request, Response } from "express";
 import { IProduct } from "./interfaces/product";
+import {messages} from './enums/messages'
 
 const app = express();
 const route = Router();
 app.use(express.json());
 
+const data = require("../product.json");
+const products: IProduct[] = data;
+
+route.get("/productList", (req: Request, res: Response) => {
+    if (products.length !== 0) {
+      res.json(products);
+    } else {
+      res.json({ message: messages.unregisteredClient });
+    }
+  });
 
 app.use(route);
 app.listen(3000, () => "server running port 3000");
