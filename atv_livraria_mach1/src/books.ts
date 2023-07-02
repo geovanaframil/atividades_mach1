2ce74c029c6a1b3499ac0f3eda779413537f52d4
Geovana Framil <geovanaframil@hotmail.com>
2023-07-02 09:20:50 -0300

    Rota get books

--- atv_livraria_mach1/src/books.ts
+++ atv_livraria_mach1/src/books.ts
@@ -1,5 +1,20 @@
 import { Router, Request, Response } from "express";
+import { IBooks } from "./interfaces/booksInterface";
 
 const route = Router();
+const data = require("../books.json");
+const books: IBooks[] = data;
+
+route.get("/", (req: Request, res: Response) => {
+  res.status(200).json({ message: "Welcome" });
+});
+
+route.get("/booksList", (req: Request, res: Response) => {
+  if (books.length !== 0) {
+    res.status(200).json(books);
+  } else {
+    res.status(404).json({ message: "No registered books" });
+  }
+});
 
 export default route;
