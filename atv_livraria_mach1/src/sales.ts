1085fab15e3457a6d987de300f6c1b9fbad5254f
Geovana Framil <geovanaframil@hotmail.com>
2023-07-06 20:29:42 -0300

    Rota get vendas por data da venda

--- atv_livraria_mach1/src/sales.ts
+++ atv_livraria_mach1/src/sales.ts
@@ -9,9 +9,38 @@ async function getSales() {
   return salesBD;
 }
 
+async function getSaleById(id: number) {
+  const query =
+    "select id_venda, total, data_venda as saleDate from vendas where id_venda=$1";
+  const params = [id];
+  const user = await executeQuery(query, params);
+  return user;
+}
+
+async function getSalesByDate(startDate: any, endDate: any) {
+  const query =
+    "select id_venda, total, data_venda as saleDate from vendas where data_venda >= $1 and data_venda <= $2";
+  const params = [startDate, endDate];
+  const sale = await executeQuery(query, params);
+  return sale;
+}
+
 route.get("/salesList", async (req: Request, res: Response) => {
   const sales = await getSales();
   res.status(200).json(sales);
+});
+
+route.get("/sale/:id", async (req: Request, res: Response) => {
+  const saleId = Number(req.params.id);
+  const sale = await getSaleById(saleId);
+  res.status(200).json(sale);
+});
+
+route.get("/salesDate", async (req: Request, res: Response) => {
+  const startDate = req.query.startDate;
+  const endDate = req.query.endDate;
+  const sale = await getSalesByDate(startDate, endDate);
+  res.status(200).json(sale);
 });
 
 export default route;
